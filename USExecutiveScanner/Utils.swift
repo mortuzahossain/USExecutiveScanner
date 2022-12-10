3fbc6ee842f0a722482715a75b4832582c6ad4e0
Mortuza Hossain <mortuzahossain1997@gmail.com>
2022-12-10 18:47:08 +0600

    Added QR Code scanner

---
+++ USExecutiveScanner/Utils.swift
@@ -0,0 +1,43 @@
+//
+//  Utils.swift
+//  USExecutiveScanner
+//
+//  Created by Mortuza Hossain on 10/12/22.
+//
+
+import Foundation
+import UIKit
+
+let hudView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+let indicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
+extension UIViewController {
+    func showHUD() {
+        hudView.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
+        hudView.backgroundColor = UIColor.darkGray
+        hudView.alpha = 0.9
+        hudView.layer.cornerRadius = hudView.bounds.size.width/2
+        indicatorView.center = CGPoint(x: hudView.frame.size.width/2, y: hudView.frame.size.height/2)
+        indicatorView.style = UIActivityIndicatorView.Style.medium
+        indicatorView.color = UIColor.white
+        hudView.addSubview(indicatorView)
+        indicatorView.startAnimating()
+        view.addSubview(hudView)
+    }
+    func hideHUD() {
+        hudView.removeFromSuperview()
+    }
+
+}
+
+extension UIViewController {
+    func hideKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
--- USExecutiveScanner/ViewController.swift
+++ USExecutiveScanner/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,6 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnScan: UIButton!
     @IBOutlet weak var btnManualInput: UIButton!
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    lazy var readerVC: QRCodeReaderViewController = {
+        let builder = QRCodeReaderViewControllerBuilder {
+            $0.reader = QRCodeReader(metadataObjectTypes: [.qr], captureDevicePosition: .back)
+            $0.showTorchButton        = true
+            $0.showSwitchCameraButton = false
+            $0.showCancelButton       = true
+            $0.showOverlayView        = true
+            $0.rectOfInterest         = CGRect(x: 0.2, y: 0.2, width: 0.6, height: 0.6)
+        }
+        
+        return QRCodeReaderViewController(builder: builder)
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +49,7 @@ class ViewController: UIViewController {
     
 
     @IBAction func clickOnScan(_ sender: Any) {
-        goToDetailsPage()
+        openQRScanner()
     }
     
     @IBAction func clickOnManualInput(_ sender: Any) {
@@ -45,6 +61,18 @@ class ViewController: UIViewController {
         self.present(vc, animated: true, completion: nil)
     }
     
+    func openQRScanner(){
+        guard checkScanPermissions() else { return }
+        readerVC.delegate = self
+        
+        readerVC.completionBlock = { (result: QRCodeReaderResult?) in
+            guard let text = result?.value else { return }
+            self.performApiCall(zipcode: text)
+        }
+
+        readerVC.modalPresentationStyle = .fullScreen
+        present(readerVC, animated: true, completion: nil)
+    }
     
     func showManualInputDialog(){
         let alert = UIAlertController(title: "Manual Input", message: nil, preferredStyle: .alert)
@@ -66,6 +94,18 @@ class ViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    func getSortType() -> String {
+        let st = sortSegmentedControl.selectedSegmentIndex
+        if st == 0 {
+            return "1"
+        } else {
+            return "2"
+        }
+    }
+    
+}
+
+extension ViewController{
     func performApiCall(zipcode:String){
         showHUD()
         let dict:[String:String] = ["zipcode": zipcode, "sorttype": getSortType()]
@@ -83,59 +123,60 @@ class ViewController: UIViewController {
                     }
                 } else {
                     DispatchQueue.main.async {
-                        Loaf(decodedData.msg, state: .error, location: .top, sender: self).show()
+                        Loaf(decodedData.msg, state: .error, location: .bottom, sender: self).show()
                     }
                 }
             } catch {
                 DispatchQueue.main.async {
-                    Loaf("No Package found", state: .error, location: .top, sender: self).show()
+                    Loaf("No Package found", state: .error, location: .bottom, sender: self).show()
                 }
             }
         }
     }
-    
-    func getSortType() -> String {
-        let st = sortSegmentedControl.selectedSegmentIndex
-        if st == 0 {
-            return "1"
-        } else {
-            return "2"
-        }
-    }
-    
 }
 
-let hudView = UIView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
-let indicatorView = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
-extension UIViewController {
-    func showHUD() {
-        hudView.center = CGPoint(x: view.frame.size.width/2, y: view.frame.size.height/2)
-        hudView.backgroundColor = UIColor.darkGray
-        hudView.alpha = 0.9
-        hudView.layer.cornerRadius = hudView.bounds.size.width/2
-        indicatorView.center = CGPoint(x: hudView.frame.size.width/2, y: hudView.frame.size.height/2)
-        indicatorView.style = UIActivityIndicatorView.Style.medium
-        indicatorView.color = UIColor.white
-        hudView.addSubview(indicatorView)
-        indicatorView.startAnimating()
-        view.addSubview(hudView)
-    }
-    func hideHUD() {
-        hudView.removeFromSuperview()
+extension ViewController:QRCodeReaderViewControllerDelegate{
+    
+    func reader(_ reader: QRCodeReaderViewController, didScanResult result: QRCodeReaderResult) {
+        reader.stopScanning()
+        dismiss(animated: true, completion: nil)
     }
 
-}
+    func reader(_ reader: QRCodeReaderViewController, didSwitchCamera newCaptureDevice: AVCaptureDeviceInput) {
+    }
 
-extension UIViewController {
-    func hideKeyboard() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
+    func readerDidCancel(_ reader: QRCodeReaderViewController) {
+        reader.stopScanning()
+        dismiss(animated: true, completion: nil)
     }
     
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
+    private func checkScanPermissions() -> Bool {
+       do {
+         return try QRCodeReader.supportsMetadataObjectTypes()
+       } catch let error as NSError {
+         let alert: UIAlertController
+
+         switch error.code {
+         case -11852:
+           alert = UIAlertController(title: "Error", message: "This app is not authorized to use Back Camera.", preferredStyle: .alert)
+
+           alert.addAction(UIAlertAction(title: "Setting", style: .default, handler: { (_) in
+             DispatchQueue.main.async {
+               if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+                 UIApplication.shared.openURL(settingsURL)
+               }
+             }
+           }))
+
+           alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+         default:
+           alert = UIAlertController(title: "Error", message: "Reader not supported by the current device", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+         }
+
+         present(alert, animated: true, completion: nil)
+
+         return false
+       }
+     }
 }
-
-
